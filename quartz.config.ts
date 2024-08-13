5a22771c57dad86098f34d159ce83212defb409d
00xSEV <147788519+00xSEV@users.noreply.github.com>
2024-08-13 14:17:16 +0700

    Fix links on 404 page by updating baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "00xsev.github.io/homepage",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
